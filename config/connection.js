aef3c6d1f6363c35f128cb5d64226c84a9da431e
carolynlupi <karolynlupi@gmail.com>
2023-08-21 23:12:31 -0400

    Added more to gitignore

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,22 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-// connect to db with mysql, use .env file to secure password (also added to .gitignore)
-const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        port: '3306',
-        user: 'root',
-        password: process.env.PASSWORD,
-        database: 'employee_db'
-    },
-    // confirm connection to db with yellow console log in terminal
-    console.log('\u001b[33m', `Connected to the employee_db database ✔`)
-); 
+// Connect to the database using MySQL, utilizing .env file for secure password (also added to .gitignore)
+const db = mysql.createConnection({
+    host: 'localhost',
+    port: 3306, 
+    user: 'root',
+    password: process.env.PASSWORD,
+    database: 'employee_db'
+});
+
+// Confirm connection to the database with a console log in the terminal
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the employee_db database ✔');
+});
 
 module.exports = db;
